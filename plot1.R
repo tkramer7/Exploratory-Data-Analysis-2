e5ccdcac37901a9f6484fdff4badb70bb4898855
Terrill Kramer <tkramer7@gmail.com>
2015-10-24 10:13:33 -0700

    add files

---
+++ plot1.R
@@ -0,0 +1,9 @@
+NEI <- readRDS("./summarySCC_PM25.rds")
+SCC <- readRDS("./Source_Classification_Code.rds")
+
+PM25TotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('plot1.png')
+barplot(height=PM25TotalByYear$Emissions, names.arg=PM25TotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions from 1999 to 2008'))
+
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,22 @@
+if(!exists("NEI")){
+    NEI <- readRDS("./data/summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+    SCC <- readRDS("./data/Source_Classification_Code.rds")
+}
+
+library(ggplot2)
+
+# Baltimore is fips code 24510 
+baltimore  <- NEI[NEI$fips=="24510", ]
+
+PM25TotalByYear <- aggregate(Emissions ~ year + type, baltimore, sum)
+
+png("plot3.png", width=640, height=480)
+g <- ggplot(PM25TotalByYear, aes(year, Emissions, color = type))
+g <- g + geom_line() +
+    xlab("year") +
+    ylab(expression('Total PM'[2.5]*" Emissions")) +
+    ggtitle('Total Emissions in Baltimore, MD (fips == "24510") from 1999 to 2008')
+print(g)
+dev.off()
