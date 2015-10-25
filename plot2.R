b1043a8172d2a1bea71c2c2dba1b0b65ae92c24d
Terrill Kramer <tkramer7@gmail.com>
2015-10-24 21:24:03 -0700

    updated files

--- plot2.R
+++ plot2.R
@@ -1,11 +1,19 @@
-NEI <- readRDS("./summarySCC_PM25.rds")
-SCC <- readRDS("./Source_Classification_Code.rds")
+if(!exists("NEI")){
+    NEI <- readRDS("./summarySCC_PM25.rds")
+}
+
+if(!exists("SCC")){
+    SCC <- readRDS("./Source_Classification_Code.rds")
+}
 
 baltimore  <- NEI[NEI$fips=="24510", ]
 
 PM25TotalByYear <- aggregate(Emissions ~ year, baltimore, sum)
 
 png('plot2.png')
-barplot(height=PM25TotalByYear$Emissions, names.arg=PM25TotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in Baltimore, MD emissions from 1999 to 2008'))
+barplot(height=PM25TotalByYear$Emissions, col=c("lightblue", "mistyrose", "lightcyan",
+                                                  "lavender"), names.arg=PM25TotalByYear$year, xlab="years", 
+        ylab=expression('total PM'[2.5]*' emission'), 
+        main=expression('Total PM'[2.5]*' in Baltimore, MD emissions from 1999 to 2008'))
 
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,8 +1,8 @@
 if(!exists("NEI")){
-    NEI <- readRDS("./data/summarySCC_PM25.rds")
+    NEI <- readRDS("./summarySCC_PM25.rds")
 }
 if(!exists("SCC")){
-    SCC <- readRDS("./data/Source_Classification_Code.rds")
+    SCC <- readRDS("./Source_Classification_Code.rds")
 }
 
 library(ggplot2)
