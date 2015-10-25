6e0cdbdcf7cceca9af63c36b95bb1bb4393511d6
Terrill Kramer <tkramer7@gmail.com>
2015-10-24 20:41:10 -0700

    add remaining files

---
+++ plot6.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+if(!exists("NEI")){
+    NEI <- readRDS("./data/summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+    SCC <- readRDS("./data/Source_Classification_Code.rds")
+}
+# merge the two data sets 
+#if(!exists("mergedNEISCC")){
+#    mergedNEISCC <- merge(NEI, SCC, by="SCC")
+#}
+
+# Baltimore is 24510, Los Angeles is 06037
+fipsNEI <- NEI[(NEI$fips=="24510"|NEI$fips=="06037") & NEI$type=="ON-ROAD",  ]
+
+PM25TotalByYearAndFips <- aggregate(Emissions ~ year + fips, fipsNEI, sum)
+PM25TotalByYearAndFips$fips[PM25TotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
+PM25TotalByYearAndFips$fips[PM25TotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
+
+png("plot6.png", width=1040, height=480)
+g <- ggplot(PM25TotalByYearAndFips, aes(factor(year), fill=year, Emissions))
+g <- g + facet_grid(. ~ fips)
+g <- g + geom_bar(stat="identity")  +
+    xlab("year") +
+    ylab(expression('Total PM'[2.5]*" Emissions")) +
+    ggtitle('Total Emissions from motor vehicle in Baltimore, MD vs Los Angeles, CA 1999 to 2008')
+print(g)
+dev.off()
